73bbf839c15f6c851d139e3c76c3993e98df9ee3
Rod Paddock <rodneypaddock@gmail.com>
2020-05-07 07:05:19 -0500

    Create Console App

---
+++ HashUtility/HashUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace HashUtility
+{
+    public class HashUtilities
+    {
+        public static string GenerateHash(string stringToHash)
+        {
+            var crypt = new SHA512Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(stringToHash), 0,
+                Encoding.UTF8.GetByteCount(stringToHash));
+
+            foreach (var bit in crypto)
+            {
+                hash.Append(bit.ToString("x2"));
+            }
+
+            return hash.ToString();
+        }
+    }
+}
---
+++ HashUtility/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //default is to hash a string
+            var hashType = "STRING";
+            var itemToHash = args[0];
+
+            //use second parameter as type of thing to hash
+            if (args.Length == 2)
+            {
+                hashType = args[1];
+            }
+             
+            if (hashType == "FILE")
+            {
+                itemToHash = File.ReadAllText(itemToHash);
+            }
+
+            var retVal = HashUtilities.GenerateHash(itemToHash);
+
+            Console.WriteLine(retVal);
+        }
+    }
+}
